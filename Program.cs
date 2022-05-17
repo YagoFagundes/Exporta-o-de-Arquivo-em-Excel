328b949d98489b22036dce32881aa9128a4bde4f
Yago <yagofagundes11@gmail.com>
2022-05-17 19:08:27 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+var path = @"C:\Users\wtfsp\Desktop\BootCamp DIO\GFT Start #4 .NET\Aula Streams\ConsoleApp6\Entrada\usuario-exportacao.csv";
+using var sr = new StreamReader (path);
+var cabecalho = sr.ReadLine()?.Split(",");
+
+while (true)
+{
+    var registro = sr.ReadLine()?.Split(",");
+    if (registro == null) break;
+    for (int i = 0; i < registro.Length; i++)
+    {
+        Console.WriteLine($"{cabecalho[i]}: {registro[i]}");
+
+    }
+    Console.WriteLine("_______________________");
+}
+Console.WriteLine("Pesione o [Enter] para finalizar!");
+Console.ReadLine();
